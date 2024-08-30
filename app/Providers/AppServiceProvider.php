c0e213468de4606860d307858b8692aaa1f6f2b7
houdaslassi <houdaslassi.18@gmail.com>
2024-08-30 23:04:52 +0100

    chore: delete test classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Core\Example;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Container\ServiceProvider\BootableServiceProviderInterface;
 use Spatie\Ignition\Ignition;
@@ -16,16 +15,14 @@ class AppServiceProvider extends AbstractServiceProvider implements BootableServ
 
     public function register(): void
     {
-        /*$this->getContainer()->add(Example::class, function () {
-            return new Example();
-        });*/
+
 
     }
 
     public function provides(String $id): bool
     {
        $services = [
-           //Example::class
+
        ];
 
        return in_array($id, $services);
